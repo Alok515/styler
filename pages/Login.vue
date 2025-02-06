44bcae6333f24af4aca849f07da73d55080f7f45
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-02-06 08:48:56 +0530

    change desine of login page

--- pages/Login.vue
+++ pages/Login.vue
@@ -30,56 +30,40 @@ const handleSignUp = (e: Event): void => {
     <div class="relative min-h-screen bg-white flex items-center justify-center">
         <Background></Background>
 
-        
-
         <div class="relative bg-white p-10 rounded-lg text-center w-[40rem] sm:w-[400px]">
             <h1 class="text-3xl font-bold font-sarina text-primary mb-6">Hello gorgeous!</h1>
-            
+
             <!-- Form Section -->
-            <form class="flex flex-col gap-4 w-full">
+            <form class="flex flex-col gap-2 w-full">
                 <!-- Email Input -->
-                <div class="flex flex-col">
-                    <label for="Email" class="text-lg font-semibold mb-2">Email</label>
-                    <input
-                        v-model="signUpFields[0].value"
-                        type="email"
-                        id="Email"
-                        name="Email"
-                        placeholder="Enter your email"
-                        class="border-none rounded bg-transparent text-gray-200 w-full focus:outline-none focus:ring-0 focus:bg-orange-700 px-4 py-2 mb-4"
-                    />
+                <div class="">
+                    <label for="Email" class="text-lg font-semibold mb-2 text-primary flex justify-start">Log in</label>
+                    <input v-model="signUpFields[0].value" type="email" id="Email" name="Email"
+                        placeholder="username/email"
+                        class="border-2 border-primary rounded bg-transparent text-gray-200 w-full focus:outline-none focus:ring-0 focus:bg-orange-700 px-4 py-2 mb-4" />
                 </div>
 
                 <!-- Password Input -->
-                <div class="flex flex-col">
-                    <label for="Password" class="text-lg font-semibold mb-2">Password</label>
-                    <input
-                        v-model="signUpFields[1].value"
-                        type="password"
-                        id="Password"
-                        name="Password"
+                <div class="">
+                    <label for="Password" class="text-lg font-semibold mb-2 text-primary flex justify-start">Password</label>
+                    <input v-model="signUpFields[1].value" type="password" id="Password" name="Password"
                         placeholder="Enter your password"
-                        class="border-none rounded bg-transparent text-gray-200 w-full focus:outline-none focus:ring-0 focus:bg-orange-700 px-4 py-2 mb-4"
-                    />
+                        class="border-2 border-primary rounded bg-transparent text-gray-200 w-full focus:outline-none focus:ring-0 focus:bg-orange-700 px-4 py-2 mb-1" />
                 </div>
 
                 <!-- Forget Password Link -->
-                <div class="flex justify-end mb-6">
-                    <a href="#" class="text-sm text-gray-500 hover:text-gray-700">Forgot your password?</a>
+                <div class="flex justify-start mb-6">
+                    <a href="#" class="text-sm text-primary">Forgot your password?</a>
                 </div>
 
                 <!-- Buttons Section -->
-                <Button 
-                    variant="outline"
-                    @click="handleSignUp"
-                    class="bg-slate-700 hover:bg-gray-700 hover:text-gray-100 mb-4"
-                >
-                    Submit
-                </Button>
-                
-                <div class="flex justify-center items-center">
-                    <span class="text-sm text-gray-500 mr-2">New User?</span>
-                    <Button variant="link" class="text-sm text-blue-500 hover:text-blue-700">Create an account</Button>
+                <div class="flex justify-between">
+                    <Button variant="outline" class=" text-white bg-primary hover:text-gray-100 px-8 mb-4">New
+                        User</Button>
+                    <Button variant="outline" @click="handleSignUp"
+                        class="bg-primary text-white hover:text-gray-100 mb-4 px-8">
+                        Submit
+                    </Button>
                 </div>
             </form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: "#EB5A3C"
+        primary: "#EC5A3C"
 
       },
       borderRadius: {
