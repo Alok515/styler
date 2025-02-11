51429ea100113146be95d243ab952f4783998e60
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-02-11 22:20:01 +0530

    added login from backend

--- components/ui/button/Background.vue
+++ components/ui/button/Background.vue
@@ -1,5 +1,5 @@
 <template>
-      <div class="absolute inset-0 z-10">
+      <div class="absolute inset-0 -z-10">
           <div class="absolute w-[250px] h-[250px] top-[6%] left-[30%] -translate-y-1/2 rounded-b-[29px] bg-lightSquareBox" ></div>
           <div class="absolute w-[340px] h-[250px] top-[231px] left-[-141px] rounded-r-[29px] bg-lightSquareBox"></div>
         <div class="absolute w-[300px] h-[300px] bottom-[-2%] right-[0%] rounded-tl-[29px]  bg-lightSquareBox"></div>
